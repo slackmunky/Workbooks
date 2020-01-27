443e706de7c4da382802c217ffa45752cdd64130
slackmunky <slackmunky@gmail.com>
2020-01-26 21:32:00 -0600

    Another commit.

--- Open projects/scratch.py
+++ Open projects/scratch.py
@@ -2,3 +2,17 @@ class Rules():
     def washing_brushes(self):
         return "Point bristles towards the basin while washing your brushes."
 
+
+class Circle():
+    pi = 3.14
+
+    def area(self, radius):
+        return self.pi * radius ** 2
+
+
+circle = Circle()
+
+pizza_area = circle.area(.5 * 12)
+print(pizza_area)
+
+
