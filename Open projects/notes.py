67d5524e76a4fc5771a373250c98b290b10aefc9
slackmunky <slackmunky@gmail.com>
2020-02-20 19:44:36 -0600

    Updated notes.py with Stacks, Queuing, and Hashing

--- Open projects/notes.py
+++ Open projects/notes.py
@@ -14,6 +14,9 @@
 # Nodes 245-ish through 324-ish
 
 # Basic class and method
+from node import Node
+
+
 class Rules:
     # noinspection PyMethodMayBeStatic
     def washing_brushes(self):
@@ -52,7 +55,7 @@ class CircleInfo:
     # "__init__" performs method whenever class is called
     def __init__(self, diameter):
         print("New circle with diameter: {diameter}".format(
-                diameter=diameter
+            diameter=diameter
         ))
 
 
@@ -87,8 +90,8 @@ alternative_rocks.store_name = "Alternative Rocks"
 isabelles_ices.store_name = "Isabelle's Ices"
 
 store_string = "{} {}".format(
-        alternative_rocks.store_name,
-        isabelles_ices.store_name)
+    alternative_rocks.store_name,
+    isabelles_ices.store_name)
 print(store_string)
 
 
@@ -248,8 +251,6 @@ class SpecialPotatoSalad(PotatoSalad):
         self.raisins = 40
 
 
-##############################################################################
-
 class Node:
     # Sets initial Node value and defaults downstream node value to None.
     def __init__(self, value, next_node=None):
@@ -327,3 +328,236 @@ linked_list.insert_beginning(70)
 linked_list.insert_beginning(5675)
 linked_list.insert_beginning(90)
 print(linked_list.stringify_list())
+
+
+class Stack:
+    def __init__(self, limit=1000):
+        self.top_item = None
+        self.size = 0
+        self.limit = limit
+
+    def push(self, value):
+        if self.has_space():
+            item = Node(value)
+            item.set_next_node(self.top_item)
+            self.top_item = item
+            self.size += 1
+            print("Adding {} to the pizza stack!".format(value))
+        else:
+            print("No room for {}!".format(value))
+
+    def pop(self):
+        if not self.is_empty():
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            print("Delivering " + item_to_remove.get_value())
+            return item_to_remove.get_value()
+        print("All out of pizza.")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.top_item.get_value()
+        print("Nothing to see here!")
+
+    def has_space(self):
+        return self.limit > self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+
+# Defining an empty pizza stack
+pizza_stack = Stack(6)
+# Adding pizzas as they are ready until we have
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+# Uncomment the push() statement below:
+pizza_stack.push("pizza #7")
+
+# Delivering pizzas from the top of the stack down
+print("The first pizza to deliver is " + pizza_stack.peek())
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+# Uncomment the pop() statement below:
+pizza_stack.pop()
+
+
+class Queue:
+    def __init__(self, max_size=None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
+
+    def enqueue(self, value):
+        if self.has_space():
+            item_to_add = Node(value)
+            print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+            if self.is_empty():
+                self.head = item_to_add
+                self.tail = item_to_add
+            else:
+                self.tail.set_next_node(item_to_add)
+                self.tail = item_to_add
+            self.size += 1
+        else:
+            print("Sorry, no more room!")
+
+    def dequeue(self):
+        if self.get_size() > 0:
+            item_to_remove = self.head
+            print(str("The " + item_to_remove.get_value()) + " is served!")
+            if self.get_size() == 1:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("The queue is totally empty!")
+
+    def peek(self):
+        if self.size > 0:
+            return self.head.get_value()
+        else:
+            print("No orders waiting!")
+
+    def get_size(self):
+        return self.size
+
+    def has_space(self):
+        if self.max_size == None:
+            return True
+        else:
+            return self.max_size > self.get_size()
+
+    def is_empty(self):
+        return self.size == 0
+
+
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+# ------------------------ #
+# Uncomment the line below:
+deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
+print("------------\nOur first order will be " + deli_line.peek())
+print("------------\nNow serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+# ------------------------ #
+# Uncomment the line below:
+deli_line.dequeue()
+# ------------------------ #
+
+
+class HashMap:
+    def __init__(self, array_size):
+        self.array_size = array_size
+        self.array = [None for item in range(array_size)]
+
+    def hash(self, key, count_collisions=0):
+        key_bytes = key.encode()
+        hash_code = sum(key_bytes)
+        return hash_code + count_collisions
+
+    def compressor(self, hash_code):
+        return hash_code % self.array_size
+
+    def assign(self, key, value):
+        array_index = self.compressor(self.hash(key))
+        current_array_value = self.array[array_index]
+
+        if current_array_value is None:
+            self.array[array_index] = [key, value]
+            return
+
+        if current_array_value[0] == key:
+            self.array[array_index] = [key, value]
+            return
+
+        number_collisions = 1
+
+        while(current_array_value[0] != key):
+            new_hash_code = self.hash(key, number_collisions)
+            new_array_index = self.compressor(new_hash_code)
+            current_array_value = self.array[new_array_index]
+
+            if current_array_value is None:
+                self.array[new_array_index] = [key, value]
+                return
+
+            if current_array_value[0] == key:
+                self.array[new_array_index] = [key, value]
+                return
+
+            number_collisions += 1
+
+        return
+
+    def retrieve(self, key):
+        array_index = self.compressor(self.hash(key))
+        possible_return_value = self.array[array_index]
+
+        if possible_return_value is None:
+            return None
+
+        if possible_return_value[0] == key:
+            return possible_return_value[1]
+
+        retrieval_collisions = 1
+
+        while (possible_return_value != key):
+            new_hash_code = self.hash(key, retrieval_collisions)
+            retrieving_array_index = self.compressor(new_hash_code)
+            possible_return_value = self.array[retrieving_array_index]
+
+            if possible_return_value is None:
+                return None
+
+            if possible_return_value[0] == key:
+                return possible_return_value[1]
+
+            retrieval_collisions += 1
+
+        return
+
+
+hash_map = HashMap(20)
+hash_map.assign("gabbro", "igneous")
+hash_map.assign("sandstone", "sedimentary")
+hash_map.assign("gneiss", "metamorphic")
+print(hash_map.retrieve("gabbro"))
+print(hash_map.retrieve("sandstone"))
+print(hash_map.retrieve("gneiss"))
